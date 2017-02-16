b877fb3c0fa9a11e97a97249294f19f79f3d833f
eytant1 <eytant1@gmail.com>
2017-02-16 12:26:52 +0000

    New Basic Data for car

--- README.md
+++
@@ -1,3 +0,0 @@
-# trafficlight
- This project is done by Eytan and Li-kesem
-	And it is the best project in the world!!
---
+++ traffic/basestation/models/BasicData.py
@@ -0,0 +1,19 @@
+ '''x are the places of the car and t is the time between them'''
+
+def Velocity(x1,x2,x3,t):
+    v2 = (x3-x1)/2t
+    return (v2)
+
+def Acceleration(x1,x2,x3,t):
+    v1 = (x2-x1)/t
+    v3 = (x3-x2)/t
+    a2 = (v3-v1)/t
+    return (a2)
+
+def ExpectedPoint(x1,x2,x3,t):
+    v2 = Velocity(x1,x2,x3,t)
+    a2 = Acceleration(x1,x2,x3,t)
+    x4 = x2+2*t*v2+2*t*t*a2
+    return (x4)
+
+
